bdb1f70cc5d41e2c05a8601b1188273213e63d73
wificampbell <ashleybrown06@icloud.com>
2026-01-04 12:29:10 -0500

    connecting to mongodb atlas

--- Server/db.js
+++ Server/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://localhost:27017"; 
+const url = "mongodb+srv://wificampbell:soulinkpassword@soulinkcluster.ec8cilb.mongodb.net/?appName=soulinkcluster";
 const client = new MongoClient(url); 
 
 let db; 
@@ -8,7 +8,7 @@ let db;
 async function connectDb(){
     await client.connect();
     db = client.db("JournalingApp");
-    console.log("Connected!"); 
+    console.log("Connected To MongoDB Atlas!"); 
 }
 
 function getDb(){
